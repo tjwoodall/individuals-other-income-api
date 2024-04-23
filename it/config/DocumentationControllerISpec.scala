dea34356166375a89396f7b875d26c656ab63d39
Chandrashekhar Korivi <14151747+sudoku007@users.noreply.github.com>
2024-04-23 13:00:11 +0100

    [MTDSA-23224] [CK] Add [Test only] to title of API and summary of all endpoints. (#12)

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -86,7 +86,7 @@ class DocumentationControllerISpec extends IntegrationBaseSpec {
         val openAPI = Option(parserResult.getOpenAPI).getOrElse(fail("openAPI wasn't defined"))
 
         openAPI.getOpenapi shouldBe "3.0.3"
-        openAPI.getInfo.getTitle shouldBe "Individuals Other Income (MTD)"
+        openAPI.getInfo.getTitle shouldBe "Individuals Other Income (MTD) [Test only]"
         openAPI.getInfo.getVersion shouldBe version.toString
       }
 
