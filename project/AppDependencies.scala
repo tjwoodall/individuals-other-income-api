48dcd47b57e8f6aa76912cb063a731532f9f58ae
Behrouz Karimi <125777824+bkarimii@users.noreply.github.com>
2026-04-10 11:41:58 +0100

    MTDSA-33864 - Update schema examples, types, and validation constraints in Individuals Other Income API documentation (#58)
    * MTDSA-33864
    
    * MTDSA-33864
    
    * MTDSA-33864
    
    * MTDSA-33864
    
    * requested corrections addressed

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "9.19.0"
+  val bootstrapPlayVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
@@ -12,14 +12,14 @@ object AppDependencies {
   )
 
   val test: Seq[sbt.ModuleID] = Seq(
-    "org.scalatestplus" %% "scalacheck-1-18"        % "3.2.19.0",
-    "org.scalamock"     %% "scalamock"              % "7.4.0",
+    "org.scalatestplus" %% "scalacheck-1-19"        % "3.2.20.0",
+    "org.scalamock"     %% "scalamock"              % "7.5.5",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapPlayVersion
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq(
-    "io.swagger.parser.v3"          % "swagger-parser-v3"    % "2.1.30",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.19.1"
+    "io.swagger.parser.v3"          % "swagger-parser-v3"    % "2.1.39",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.2"
   ).map(_ % Test)
 
 }
